669f58810ad2bdc0dcddb0e70c29a0d71b020395
Jan Jirsík <jjirsik@gmail.com>
2021-10-19 12:05:23 +0200

    getting data from DB

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Book project</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+
+    @yield('content')
+
+</body>
+</html>
